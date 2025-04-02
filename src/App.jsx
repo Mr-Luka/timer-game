750f347dbe65949292e8d084eec9506d420deaf6
Luka <luka.luka.luka93@gmail.com>
2025-04-02 12:32:29 -0700

    Starting project, made app and player component

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './index.css'
+import Player from './components/Player.jsx';
+function App() {
+
+
+  return (
+    <>
+      <Player />
+      <div className='challenges'>
+
+      </div>
+    </>
+  )
+}
+
+export default App
